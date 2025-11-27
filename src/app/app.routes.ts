8b03e0c01395859a347ba54982c912442fce25c7
Aharon Moskovitsh <f05485@gmail.com>
2025-11-27 16:35:09 +0200

    feat: refactor invoice feature to invoices, implement new component and service methods

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { MainLayout } from './layout/main-layout/main-layout';
 // אם המסכים עדיין לא קיימים – ניצור אותם עוד רגע
 import { Dashboard } from './features/dashboard/dashboard';
 import { Upload } from './features/upload/upload';
-import { Invoice } from './features/invoice/invoice';
+import { Invoices } from './features/invoices/invoices';
 import { Reports } from './features/reports/reports';
 import { Settings } from './features/settings/settings/settings';
 
@@ -16,7 +16,7 @@ export const routes: Routes = [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: Dashboard },
       { path: 'upload', component: Upload },
-      { path: 'invoices', component: Invoice },
+      { path: 'invoices', component: Invoices },
       { path: 'reports', component: Reports },
       { path: 'settings', component: Settings },
     ],
